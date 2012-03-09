f67ed01fa65e9e74a91b6c832cf109e974051415
Flavin <flavinj@gmail.com>
2012-03-08 19:44:37 -0600

    file reading added

--- clubstat.py
+++ clubstat.py
@@ -1,6 +1,7 @@
 import cards
-import sys
+from sys import argv
 import argparse
+import csv
 
 
 class SpadesGame:
@@ -18,9 +19,10 @@ parser.add_argument('n', type=int, help='Order of magnitude of runs. Will run 10
 parser.add_argument('--name', type=str, default='results.csv', \
 					help='Write to the specified file (if -f is set). If no filename is specified, writes to "results.csv".')
 
-args = parser.parse_args(sys.argv[1:])
+args = parser.parse_args(argv[1:])
 
 runs = 10**args.n
+total_runs = runs
 filename = args.name
 #write = args.f
 
@@ -29,7 +31,53 @@ filename = args.name
 club_wins = [0]*13
 club_losses = [0]*13
 
-for iters in range(runs):
+##########
+# Before we run any games, we see if games have been run before. If so, we read in that data. No use letting our old data go to waste.
+try:
+	print('Reading old data')
+	f = open(filename,'r')
+except:
+	print('Tried reading old data but it failed')
+else:
+		reader = csv.reader(f)
+		line_list = list(reader)
+		##########
+		# First Row: Get runs 
+		row = line_list[0]
+		runs_old = int(row[0])
+		total_runs += runs_old
+
+		##########
+		# Second line is card titles. Skip to third line.
+		next_index = 2
+		row = line_list[next_index]
+
+		##########
+		# Wins
+		# This row has one title space then all the 'club_wins' data.
+		for i in range(13):
+			club_wins[i] += int(row[i+1])
+
+		##########
+		# Losses
+		# This row has one title space then all the 'club_losses' data.
+		next_index += 1
+		row = line_list[next_index]
+		for i in range(13):
+			club_losses[i] += int(row[i+1])
+		
+		f.close()
+
+
+line_out = '{},Hands'.format(total_runs) \
+			+'\nlow club,2,3,4,5,6,7,8,9,10,J,Q,K,A'
+print('Starting games...')
+every_ten = 10
+for this_game in range(runs):
+	if (this_game+1) % every_ten == 0:
+		print('Played {} games'.format(this_game+1))
+		if every_ten < runs/10:
+			every_ten *= 10
 
 	# Deal out the cards
 	game = SpadesGame()
@@ -37,8 +85,8 @@ for iters in range(runs):
 	# Get the lowest card from each player's hand
 	low_cards = [min(hand) for hand in game.hands]
 
-	#Keep only clubs. (For our purposes clubs are integers 0--12.)
-	low_clubs = [card for card in low_cards if card<13]
+	#Keep only clubs.
+	low_clubs = [card for card in low_cards if card.suit==0]
 
 	#The highest club is the winner
 	winner = max(low_clubs)
@@ -50,28 +98,20 @@ for iters in range(runs):
 	for card in low_clubs:
 		club_losses[ card ] += 1
 
-# Format the results
-column_titles = ['low club','wins','losses','win v loss %\n']
-card_names = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+
 win_pct = [0]*13
 for i in range(13):
 	if club_wins[i] != 0 or club_losses[i] != 0:
 		win_pct[i] = 100.0*float(club_wins[i])/float(club_wins[i]+club_losses[i])
 	else:
 		win_pct[i] = float('nan')
-data_lines = list(zip(card_names,club_wins,club_losses,win_pct))
+
+line_out += '\nwins,'+','.join(club_wins)+'\nlosses,'+','.join(club_losses) \
+			+'\nwin %'+','.join(win_pct)
 
 # Write results to a file
-if write:
-	with open(filename,'a') as f:
-		f.write('10^{},,,\n'.format(args.n))
-		f.write(','.join(column_titles))
-		for line in data_lines:
-			f.write('{0},{1},{2},{3:.3f}\n'.format(*list(line)))
+print('Writing results to file')
+with open(filename,'w') as f:
+	f.write(line_out)
 
-
-# Print results to screen
-print("We ran {} hands. Low club stats are:".format(runs))
-
-for line in data_lines:
-	print('{3:.2f}%\t{0} won {1} times and lost {2} times.'.format(*list(line)))
+print('Results written to file '+filename)
